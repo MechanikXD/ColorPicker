bb8d8f02a62dc7cb8f7f644c704c7c2c7fad7d85
Mech <andreydolugame@gmail.com>
2026-07-09 19:08:14 +0300

    Add result color to ColorCombination model

--- ColorPicker/Models/Colors/ColorCombination.cs
+++ ColorPicker/Models/Colors/ColorCombination.cs
@@ -2,6 +2,7 @@ namespace ColorPicker.Models.Colors;
 
 public class ColorCombination
 {
+    public ColorSwatch ResultColor { get; set; } = new() { Green = byte.MaxValue };
     public ColorSwatch FirstColor { get; set; } = new() { Red = byte.MaxValue };
     public ColorSwatch SecondColor { get; set; } = new() { Blue = byte.MaxValue };
     public double FirstColorRatio { get; set; } = 0;
