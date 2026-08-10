879d5ed6f05c4e37fd4bf9141fcfe47922a61b1c
Mech <andreydolugame@gmail.com>
2026-08-10 11:52:07 +0300

    Preheat main pages for faster loading

--- ColorPicker/App.xaml.cs
+++ ColorPicker/App.xaml.cs
@@ -1,5 +1,7 @@
 using ColorPicker.Services.SaveLoad;
 using ColorPicker.Services.Theme;
+using ColorPicker.View;
+using ColorPicker.ViewModels;
 
 namespace ColorPicker;
 
@@ -11,5 +13,23 @@ public partial class App : Application
         themeService.ApplySavedTheme();
         paletteLoadService.Load();
         MainPage = appShell;
+    }
+    
+    protected override void OnStart()
+    {
+        base.OnStart();
+        
+        Task.Run(() =>
+        {
+            // Force DI container to construct heavy ViewModels/Services beforehand
+            _ = Handler?.MauiContext?.Services.GetService<MainPage>();
+            _ = Handler?.MauiContext?.Services.GetService<MainViewModel>();
+            
+            _ = Handler?.MauiContext?.Services.GetService<PalettePage>();
+            _ = Handler?.MauiContext?.Services.GetService<PaletteViewModel>();
+            
+            _ = Handler?.MauiContext?.Services.GetService<CameraPage>();
+            _ = Handler?.MauiContext?.Services.GetService<CameraViewModel>();
+        });
     }
 }
