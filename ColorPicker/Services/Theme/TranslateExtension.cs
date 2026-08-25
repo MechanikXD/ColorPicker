4542273e552fecf8dda4c94942d006bb69158a6f
Mech <andreydolugame@gmail.com>
2026-08-25 11:24:14 +0300

    Add localization and Translate Extension

---
+++ ColorPicker/Services/Theme/TranslateExtension.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using ColorPicker.Resources.Strings;
+
+namespace ColorPicker.Services.Theme;
+
+[ContentProperty(nameof(Text))]
+public class TranslateExtension : IMarkupExtension
+{
+    private const string ABSENT_KEY_MESSAGE = "NO_LOCALIZATION_KEY_FOUND";
+    public string Text { get; set; } = string.Empty;
+
+    public object ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return string.Empty;
+
+        var translation = AppResources.ResourceManager.GetString(Text, CultureInfo.CurrentUICulture);
+
+        return translation ?? ABSENT_KEY_MESSAGE; // Fallback to key name if string isn't found
+    }
+
+}
